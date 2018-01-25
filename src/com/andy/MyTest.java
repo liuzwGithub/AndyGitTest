c947253b66dc174a3a00afa6bb50399cee34d687
andy_hanna <liuzw@asiainfo.com>
2018-01-25 12:34:49 +0800

    Initial commit

---
+++ src/com/andy/MyTest.java
@@ -0,0 +1,23 @@
+package com.andy;
+
+/**
+ * Copyright: Copyright (c) 2018 Asiainfo-Linkage
+ *
+ * @ClassName:
+ * @Description: 该类的功能描述
+ * @version: v1.0.0
+ * @author: liuzw
+ * @date: 2018/1/25 12:25
+ * <p>
+ * Modification History:
+ * Date         Author          Version            Description
+ * ------------------------------------------------------------*
+ * 2018/1/25      Machenike         v1.0.0               修改原因
+ */
+public class MyTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("andy git test...");
+    }
+}
